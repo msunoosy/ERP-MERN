159fe9d49f66737c56f2cdab2c2a7c8f5969ba34
msunoosy <m.sunoos@gmail.com>
2022-03-01 19:40:36 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
+import Employeeregistration from './components/Employeeregistration';
+function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  return (
+    <div >
+       <Button type="primary" onClick={showModal}>
+        Employee Registration
+      </Button>
+      <Modal title="Basic Modal"  visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <Employeeregistration></Employeeregistration>
+      </Modal>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Employeeregistration.js
@@ -0,0 +1,211 @@
+import React from 'react'
+import { Form, Input, InputNumber, Button, DatePicker, Select, Table, Tag, Space } from 'antd';
+
+function Employeeregistration() {
+
+
+    const { Column, ColumnGroup } = Table;
+    const { Option } = Select;
+    const data = [
+        {
+          key: '1',
+          firstName: 'John',
+          lastName: 'Brown',
+          age: 32,
+          address: 'New York No. 1 Lake Park',
+          tags: ['nice', 'developer'],
+        },
+        {
+          key: '2',
+          firstName: 'Jim',
+          lastName: 'Green',
+          age: 42,
+          address: 'London No. 1 Lake Park',
+          tags: ['loser'],
+        },
+        {
+          key: '3',
+          firstName: 'Joe',
+          lastName: 'Black',
+          age: 32,
+          address: 'Sidney No. 1 Lake Park',
+          tags: ['cool', 'teacher'],
+        },
+      ];
+
+    const prefixSelector = (
+        <Form.Item name="prefix" noStyle>
+            <Select
+                style={{
+                    width: 70,
+                }}
+            >
+                <Option value="86">+94</Option>
+                <Option value="87">+87</Option>
+            </Select>
+        </Form.Item>
+    );
+
+    function onChange(date, dateString) {
+        console.log(date, dateString);
+    }
+
+    const layout = {
+        labelCol: {
+            span: 8,
+        },
+        wrapperCol: {
+            span: 8,
+        },
+    };
+
+    /* eslint-disable no-template-curly-in-string */
+
+    const validateMessages = {
+        required: '${label} is required!',
+        types: {
+            email: '${label} is not a valid email!',
+            number: '${label} is not a valid number!',
+        },
+        number: {
+            range: '${label} must be between ${min} and ${max}',
+        },
+    };
+    /* eslint-enable no-template-curly-in-string */
+
+    const onFinish = (values) => {
+        console.log(values);
+    };
+
+
+    return (
+
+        <div>
+            <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+            <Form.Item
+                    name={['user', 'id']}
+                    label="ID"
+                    rules={[
+                        {
+                            type: 'number',
+                            min: 0,
+                        },
+                    ]}
+                >
+                    <InputNumber />
+                </Form.Item>
+                <Form.Item
+                    name={['user', 'firstname']}
+                    label="First Name"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    name={['user', 'lastname']}
+                    label="Last Name"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item name={['user', 'dob']} label="Date of birth">
+                    <DatePicker onChange={onChange} />
+                </Form.Item>
+
+                <Form.Item
+                    name={['user', 'age']}
+                    label="Age"
+                    rules={[
+                        {
+                            type: 'number',
+                            min: 0,
+                            max: 99,
+                        },
+                    ]}
+                >
+                    <InputNumber />
+                </Form.Item>
+
+                <Form.Item
+                    name={['user', 'email']}
+                    label="Email"
+                    rules={[
+                        {
+                            type: 'email',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    name="phone"
+                    label="Phone Number"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your phone number!',
+                        },
+                    ]}
+                >
+                    <Input
+                        addonBefore={prefixSelector}
+                        style={{
+                            width: '100%',
+                        }}
+                    />
+                </Form.Item>
+
+
+             <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+            <Table dataSource={data}>
+    <ColumnGroup title="Name">
+      <Column title="First Name" dataIndex="firstName" key="firstName" />
+      <Column title="Last Name" dataIndex="lastName" key="lastName" />
+    </ColumnGroup>
+    <Column title="Age" dataIndex="age" key="age" />
+    <Column title="Address" dataIndex="address" key="address" />
+    <Column
+      title="Tags"
+      dataIndex="tags"
+      key="tags"
+      render={tags => (
+        <>
+          {tags.map(tag => (
+            <Tag color="blue" key={tag}>
+              {tag}
+            </Tag>
+          ))}
+        </>
+      )}
+    />
+    <Column
+      title="Action"
+      key="action"
+      render={(text, record) => (
+        <Space size="middle">
+          <a>Invite {record.lastName}</a>
+          <a>Delete</a>
+        </Space>
+      )}
+    />
+  </Table>,
+        </div>
+    )
+}
+
+export default Employeeregistration
