88af061078f8d44e47ea5058835608117cf7fd20
msunoosy <m.sunoos@gmail.com>
2022-04-01 10:41:39 +0530

    Search component created

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Search.css"
 import EmployeeView from './EmployeeView';
 import { Table, Space, Popconfirm, message, Modal, Input, Button, Form } from 'antd';
 
 
-function Search() {
+function Search(props) {
+ 
+const[search,setSearch]=useState("")
 
+const searchOnclick=()=>{
+  console.log(search)
+  setSearch("")
+}
 
   return (
     <div>
-      <h1>Employee Search</h1>
-
+     <h1>Employee Search</h1>
       <Space>
-        <Input></Input>
-        <Button>Search</Button>
+      
+        <Input value={search} onChange={e=>{
+          setSearch(e.target.value)
+        }}></Input>
+        <Button onClick={searchOnclick}>Search</Button>
       </Space>
-    
-
+   <EmployeeView/> 
     </div>
   )
 }
