67e230b178bb9bac384ffc7ccd6f3ac901ec85b8
msunoosy <m.sunoos@gmail.com>
2022-04-17 16:32:31 +0530

    Login validation added

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,97 +1,114 @@
-import React, {useState} from 'react'
-import { Form, Input, Button, Checkbox,Space,message} from 'antd';
+import React, { useState } from 'react'
+import { Form, Input, Button, Checkbox, Space, message } from 'antd';
+import axios from 'axios';
 import "./login.css"
 
+
+
 function LoginPage(props) {
+  const [loginData, setLoginData] = useState([]);
   const [form] = Form.useForm();
-
-    const onFinish = (values) => {
-      if(values.username === 'um' && values.password === 'pm'){
-        props.onLogin(1)
-        form.resetFields();
-        message.success("Login Success")
-    }else{
-        props.onLogin(0)
-        form.resetFields();
+  const onFinish = (values) => {
+    axios.get(`http://localhost:8081/api/employees?email=${values.email}`).then((res) => {
+      setLoginData(res.data)
+      if(res.data.length===0){
+        console.log("No response ",res.data)
         message.error("Invalid Username or Password")
+        form.resetFields();
+      }else{
+      loginData.map((val => {
+        if (values.email === val.email && values.password === val.password) {
+          props.onLogin(1)
+          form.resetFields();
+          message.success("Login Success")
+        } else {
+          props.onLogin(0)
+          form.resetFields();
+          message.error("Invalid Username or Password")
+        }
+      
+      }))
       }
-    }
-      const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-      };
+    }).catch(err => console.log(err))
+  }
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
 
-    return (
-      <div className='container'>
-        <div className='login'>
-          <Space >
-        <Form  form={form}
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
 
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+  return (
+    <div className='container'>
+      <div className='login'>
+        <Space >
+          <Form form={form} onFinish={onFinish}
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            initialValues={{
+              remember: true,
+            }}
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+            
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
 
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Login
-        </Button>
-      </Form.Item>
+            <Form.Item
+              label="Username"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-    </Form>
-    </Space>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit" >
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </Space>
+      </div>
     </div>
-    </div>
-    )
+  )
 }
 export default LoginPage
